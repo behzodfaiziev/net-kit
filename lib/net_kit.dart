adb28ac4e9bac5e128f4d557f09a88e4821aab4d
Behzod <behzod.faiziev@gmail.com>
2024-08-20 01:54:58 +0300

    format fix

--- lib/net_kit.dart
+++ lib/net_kit.dart
@@ -3,8 +3,10 @@ library net_kit;
 
 /// Http Request Type
 export 'src/enum/request_method.dart';
+
 /// Manager
 export 'src/manager/i_net_kit_manager.dart';
 export 'src/manager/net_kit_manager.dart';
+
 /// Model
 export 'src/model/i_net_kit_model.dart';
