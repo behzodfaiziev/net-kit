226c373a2e2a09fcfdf64da22e0f2b0fe0b6c4ea
Behzod <behzod.faiziev@gmail.com>
2024-08-20 01:32:40 +0300

    exported request_method

--- lib/net_kit.dart
+++ lib/net_kit.dart
@@ -1,9 +1,10 @@
 /// NetKit is a library for network operations.
 library net_kit;
 
+/// Http Request Type
+export 'src/enum/request_method.dart';
 /// Manager
 export 'src/manager/i_net_kit_manager.dart';
 export 'src/manager/net_kit_manager.dart';
-
 /// Model
 export 'src/model/i_net_kit_model.dart';
