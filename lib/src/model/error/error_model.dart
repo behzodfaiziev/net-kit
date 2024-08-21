9510498bef638a7acd5b252047e1f98206b067f6
Behzod <behzod.faiziev@gmail.com>
2024-08-21 22:52:27 +0300

    equatable dependency removed

--- lib/src/model/error/error_model.dart
+++ lib/src/model/error/error_model.dart
@@ -1,14 +1,27 @@
-import 'package:equatable/equatable.dart';
-
+import '../../enum/http_status_codes.dart';
 import '../../utility/typedef/request_type_def.dart';
 
-class ErrorModel extends Equatable {
+/// The error model class
+/// It contains the status code and message of the error
+/// It is used to parse the error response from the server
+/// and display the error message to the user
+class ErrorModel {
+  /// The constructor for the ErrorModel class
   const ErrorModel({
     required this.statusCode,
     required this.message,
     this.messages,
   });
 
+  /// The factory method to parse the error response
+  /// It takes in the following parameters:
+  /// - [json]: The JSON response from the server
+  /// - [statusCodeKey]: The key to use for the status code parsing
+  /// - [messageKey]: The key to use for the error message parsing
+  /// - [statusCode]: The status code of the error
+  /// It returns an ErrorModel object
+  /// If the error response cannot be parsed, it returns a default error message
+  /// with a status code of 400
   factory ErrorModel.fromJson({
     required MapType? json,
     required String statusCodeKey,
@@ -37,27 +50,32 @@ class ErrorModel extends Equatable {
           singleMessage = multipleMessages[0];
         }
       }
-      final int? status = statusCode ?? json[statusCodeKey] as int?;
+      final status = statusCode ?? json[statusCodeKey] as int?;
 
       return ErrorModel(
-        statusCode: status ?? 400,
+        statusCode: status ?? HttpStatuses.badRequest.code,
         message: (singleMessage ?? '').isNotEmpty
             ? singleMessage
             : 'Could not parse the error',
         messages: multipleMessages,
       );
     } catch (e) {
-      return const ErrorModel(
-        statusCode: 400,
+      return ErrorModel(
+        statusCode: HttpStatuses.badRequest.code,
         message: 'Could not parse the error',
       );
     }
   }
 
+  /// The status code of the error
+  /// It is used to determine the type of error
   final int? statusCode;
-  final String? message;
-  final List<String>? messages;
 
-  @override
-  List<Object?> get props => [statusCode, message, messages];
+  /// The error message, which can be used to show the error to the user
+  final String? message;
+
+  /// The list of error messages
+  /// Sometimes, the server returns multiple error messages
+  /// so it handles them as a list of strings
+  final List<String>? messages;
 }
