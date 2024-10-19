d7cc00d472c21f5be9d527abf83dc2a3ac57c23b
Behzod <behzod.faiziev@gmail.com>
2024-10-20 00:03:36 +0300

    error handling interceptor done

--- packages/net-kit/lib/src/manager/interceptors/error_handling_interceptor.dart
+++ packages/net-kit/lib/src/manager/interceptors/error_handling_interceptor.dart
@@ -1,137 +1,60 @@
 part of '../net_kit_manager.dart';
 
-/// An interceptor that handles error responses and token refresh logic.
-///
-/// The `ErrorHandlingInterceptor` class is responsible for intercepting error
-/// responses, particularly 401 Unauthorized errors, and handling token refresh
-/// logic. It uses a `RequestQueue` to queue requests while the token is being
-/// refreshed.
 class ErrorHandlingInterceptor {
-  /// Creates an instance of `ErrorHandlingInterceptor`.
-  ///
-  /// The [netKitManager] parameter is the instance of `NetKitManager` used to
-  /// make network requests. The [refreshTokenPath] parameter is the endpoint
-  /// used to refresh the token.
+
   ErrorHandlingInterceptor({
-    required NetKitManager netKitManager,
-    required RequestQueue requestQueue,
-    required String? refreshTokenPath,
-  })  : _netKitManager = netKitManager,
-        _requestQueue = requestQueue,
-        _refreshTokenPath = refreshTokenPath;
-
-  /// Instance of `NetKitManager`. Used to make network requests.
-  final NetKitManager _netKitManager;
-
-  /// The endpoint used to refresh the token.
-  /// By default, the value is null.
-  /// If the value is null, the interceptor will not refresh the token.
-  final String? _refreshTokenPath;
-
-  /// Instance of `RequestQueue`. Used to queue
-  /// requests while refreshing the token.
-  final RequestQueue _requestQueue;
-
-  /// Indicates whether the token is being refreshed.
-  /// By default, the value is false.
-  /// If the value is true, the interceptor will queue
-  /// requests while refreshing the token.
-  /// If the value is false, the interceptor will refresh
-  /// the token and retry the original request.
-  /// The value is set to true while refreshing the token
-  /// and set to false after refreshing the token.
+    required this.refreshTokenPath,
+    required this.requestQueue,
+    required this.tokenManager,
+  });
+  final String? refreshTokenPath;
+  final RequestQueue requestQueue;
   bool _isRefreshing = false;
+  final TokenManager tokenManager;
 
-  /// Adds the interceptor to the `NetKitManager`.
-  ///
-  /// This method is called internally to add the interceptor to the
-  /// `NetKitManager`. It should not be called directly.
   ErrorInterceptor getErrorInterceptor() {
     return ErrorInterceptor(
       onError: (DioException error, ErrorInterceptorHandler handler) async {
-        if (error.response?.statusCode != 401 || _refreshTokenPath == null) {
+        if (error.response?.statusCode != 401 || refreshTokenPath == null) {
           return handler.next(error);
         }
+
         if (_isRefreshing) {
           // Queue the request while refreshing the token
-          _requestQueue.add(() => _retryRequest(error, handler));
+          requestQueue.add(() => _retryRequest(error, handler));
           return;
         }
 
         _isRefreshing = true;
-        await _refreshToken(
-          error: error,
-          handler: handler,
-          refreshTokenPath: _refreshTokenPath!,
-        );
-        _isRefreshing = false;
-        await _requestQueue.processQueue();
+        try {
+          await tokenManager.refreshTokens(refreshTokenPath!);
+          await _retryRequest(error, handler);
+          // Token, process the request queue
+          await requestQueue.processQueue();
+        } catch (e) {
+          handler.reject(
+            DioException(requestOptions: error.requestOptions, error: e),
+          );
+          // Reject all queued requests
+          requestQueue.rejectQueuedRequests();
+          } finally {
+          _isRefreshing = false;
+        }
       },
     );
-  }
-
-  /// Refreshes the authentication token.
-  ///
-  /// This method is called internally when a 401 Unauthorized error is
-  /// encountered. It makes a request to the [refreshTokenPath] to get a new
-  /// token and retries the original request.
-  ///
-  /// The [error] parameter is the original error that triggered the token
-  /// refresh. The [handler] parameter is used to handle the response.
-  Future<void> _refreshToken({
-    required String refreshTokenPath,
-    required DioException error,
-    required ErrorInterceptorHandler handler,
-  }) async {
-    try {
-      final refreshToken = _netKitManager.getRefreshToken();
-
-      final refreshResponse = await _netKitManager.request<dynamic>(
-        refreshTokenPath,
-        options: Options(
-          method: RequestMethod.post.name.toUpperCase(),
-        ),
-        data: {_netKitManager.parameters.refreshTokenKey: refreshToken},
-      );
-
-      final authToken = _netKitManager.extractTokens(
-        response: refreshResponse,
-        accessTokenKey: _netKitManager.parameters.accessTokenKey,
-        refreshTokenKey: _netKitManager.parameters.refreshTokenKey,
-      );
-
-      _netKitManager
-        ..addBearerToken(authToken.accessToken)
-        ..addRefreshToken(authToken.refreshToken)
-
-        // Notify about token updates (optional)
-        .._onTokensUpdated(authToken);
-
-      // Retry the original request
-      await _retryRequest(error, handler);
-    } catch (e) {
-      return handler.reject(
-        DioException(
-          requestOptions: error.requestOptions,
-          error: e,
-        ),
-      );
-    }
   }
 
   Future<void> _retryRequest(
     DioException error,
     ErrorInterceptorHandler handler,
   ) async {
-    final response = await _netKitManager.request<dynamic>(
-      error.requestOptions.path,
-      options: Options(
-        method: error.requestOptions.method,
-        headers: error.requestOptions.headers,
-      ),
-      data: error.requestOptions.data,
-      queryParameters: error.requestOptions.queryParameters,
-    );
-    return handler.resolve(response);
+    try {
+      final response = await tokenManager.retryRequest(error.requestOptions);
+      handler.resolve(response);
+    } catch (e) {
+      handler
+          .reject(DioException(requestOptions: error.requestOptions, error: e));
+    }
   }
+
 }
