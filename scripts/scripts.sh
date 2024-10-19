2669950b07c6a97fc104be9e6827cbeaffc0625a
Behzod <behzod.faiziev@gmail.com>
2024-10-20 00:22:19 +0300

    updated script

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -5,4 +5,4 @@ cd flutter_integration_test && flutter pub get && cd .. && cd packages/net-kit &
 cd flutter_integration_test && flutter run -d web-server
 
 # Test the package
-cd packages/net-kit && dart test && cd .. && cd ..
+cd packages/net-kit && dart format --set-exit-if-changed . && dart analyze --fatal-infos --fatal-warnings && dart test && cd .. && cd ..
