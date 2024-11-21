7976ca7594a6a079276654603c6caa49310597bd
Behzod <behzod.faiziev@gmail.com>
2024-11-21 03:48:02 +0300

    added testing script

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,8 +1,10 @@
 # Install the dependencies
 cd flutter_integration_test && flutter pub get && cd .. && cd packages/net-kit && flutter pub get && cd .. && cd ..
 # Run chrome app in Github Codespaces
-
 cd flutter_integration_test && flutter run -d web-server
+
+# Integration test the package
+cd flutter_integration_test && flutter test integration_test
 
 # Test the package
 cd packages/net-kit && dart format --set-exit-if-changed . && dart analyze --fatal-infos --fatal-warnings && dart test && cd .. && cd ..
