3f66c377b13c379cbe70db0738922e2077886fea
Behzod <behzod.faiziev@gmail.com>
2024-12-16 03:00:24 +0300

    fixed warning

--- packages/net-kit/lib/src/manager/net_kit_manager.dart
+++ packages/net-kit/lib/src/manager/net_kit_manager.dart
@@ -11,8 +11,8 @@ import '../utility/logger/i_net_kit_logger.dart';
 import '../utility/logger/void_logger.dart';
 import '../utility/typedef/request_type_def.dart';
 import 'adapter/io_http_adapter.dart'
-if (dart.library.io) 'adapter/io_http_adapter.dart'
-if (dart.library.html) 'adapter/web_http_adapter.dart';
+    if (dart.library.io) 'adapter/io_http_adapter.dart'
+    if (dart.library.html) 'adapter/web_http_adapter.dart';
 import 'error/api_exception.dart';
 import 'i_net_kit_manager.dart';
 import 'params/net_kit_error_params.dart';
@@ -190,7 +190,7 @@ class NetKitManager extends INetKitManager
         throw _notMapTypeError(response);
       }
       final parsedModel =
-      _converter.toModel<R>(response.data as MapType, model);
+          _converter.toModel<R>(response.data as MapType, model);
 
       return parsedModel;
     } on DioException catch (error) {
@@ -398,7 +398,7 @@ class NetKitManager extends INetKitManager
       accessTokenKey: accessTokenKey,
       refreshTokenKey: refreshTokenKey,
       internetStatusSubscription: internetStatusStream?.listen(
-            (event) {
+        (event) {
           /// Update the internet status when the stream emits a new value
           _internetEnabled = event;
         },
