8376a8fc468cd1c6cbf7c52bff1b86ded51be8ef
Behzod <behzod.faiziev@gmail.com>
2025-04-03 14:45:34 +0300

    removed unused type definitions

--- packages/net-kit/lib/src/utility/typedef/request_type_def.dart
+++ packages/net-kit/lib/src/utility/typedef/request_type_def.dart
@@ -1,18 +1,2 @@
-import '../../../net_kit.dart';
-
 /// The map type definition
 typedef MapType = Map<String, dynamic>;
-
-/// The request type definition
-typedef NetKitRequestFn = Future<Map<String, dynamic>> Function({
-  required String path,
-  Map<String, dynamic>? body,
-  Map<String, String>? headers,
-});
-
-/// The request type definition
-typedef RefreshTokenRequestBuilder = Future<AuthTokenModel> Function({
-  required String refreshTokenPath,
-  required String refreshToken,
-  required NetKitRequestFn sendRequest,
-});
